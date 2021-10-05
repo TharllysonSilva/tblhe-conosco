bb1ca0c3d318cf6364bb84a7e06438c730d85ba6
TharllysonSilva <tharllysonsilva@gmail.com>
2021-10-05 08:34:51 -0300

    dbo

--- trabalheconosco.sql
+++ trabalheconosco.sql
@@ -1,23 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
---
 -- Host: localhost
--- Generation Time: 22-Set-2021 às 14:04
--- Versão do servidor: 5.7.20-0ubuntu0.16.04.1
--- PHP Version: 7.0.22-0ubuntu0.16.04.1
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `FCO_trabalheconosco`
+-- Database: `trabalheconosco`
 --
 DROP DATABASE `trabalheconosco`;
 CREATE DATABASE IF NOT EXISTS `trabalheconosco` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
