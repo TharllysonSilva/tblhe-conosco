e42bff254903f21d41e468b3dadf0d5de02606c5
TharllysonSilva <tharllysonsilva@gmail.com>
2021-10-05 10:57:19 -0300

    menu

--- navbar_logout.php
+++ navbar_logout.php
@@ -10,12 +10,12 @@
 	<nav class="navbar navbar-default navbar-fixed-bottom">
 		<div class="container-fluid">
 			<div class="navbar-left">
-				<a href="http://www.sankyu.com.br" target="_blank">
+				<a href="" target="_blank">
 					<img class="img-responsive" src="imagens/logonovo.png" style="height: 55px;float: left; padding-top: 10px;">
 				</a>
 			</div>     
 			<div class="navbar-right">
-				<a href="http://webmail.sankyu.com.br" target="_blank">
+				<a href="" target="_blank">
 					<img class="img-responsive" src="imagens/logoTI.png" style="height: 55px;float: right;">
 				</a>
 			</div> 
